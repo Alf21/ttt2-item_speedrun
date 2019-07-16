48f9d7cac76a43d01fa59aac1b37b999d503cb81
Alf Müller <4lf-mueller@gmx.de>
2019-07-16 14:58:13 +0200

    Initial commit

---
+++ lua/terrortown/entities/items/item_ttt_speedrun.lua
@@ -0,0 +1,24 @@
+if SERVER then
+	AddCSLuaFile()
+
+	resource.AddFile("materials/vgui/ttt/icon_speedrun.vmt")
+	resource.AddFile("materials/vgui/ttt/perks/hud_speedrun.png")
+end
+
+ITEM.hud = Material("vgui/ttt/perks/hud_speedrun.png")
+ITEM.EquipMenuData = {
+	type = "item_passive",
+	name = "Speedrun",
+	desc = "You are 50% faster!"
+}
+ITEM.material = "vgui/ttt/icon_speedrun"
+ITEM.notBuyable = true
+ITEM.CanBuy = {ROLE_TRAITOR, ROLE_DETECTIVE}
+
+if SERVER then
+	hook.Add("TTTPlayerSpeedModifier", "SpeedrunModifySpeed", function(ply, _, _, noLag)
+		if IsValid(ply) and ply:HasEquipmentItem("item_ttt_speedrun") then
+			noLag[1] = noLag[1] * 1.5 * (ply.speedrun_mul or 1)
+		end
+	end)
+end
