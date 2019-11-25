e7c94dac8c6c3ff3476a87931c8b072684a66cd7
Alf Müller <4lf-mueller@gmx.de>
2019-11-25 14:21:43 +0100

    fixed prediction issues

--- lua/terrortown/entities/items/item_ttt_speedrun.lua
+++ lua/terrortown/entities/items/item_ttt_speedrun.lua
@@ -14,10 +14,8 @@ ITEM.EquipMenuData = {
 ITEM.material = "vgui/ttt/icon_speedrun"
 ITEM.CanBuy = {ROLE_TRAITOR, ROLE_DETECTIVE}
 
-if SERVER then
-	hook.Add("TTTPlayerSpeedModifier", "SpeedrunModifySpeed", function(ply, _, _, noLag)
-		if IsValid(ply) and ply:HasEquipmentItem("item_ttt_speedrun") then
-			noLag[1] = noLag[1] * 1.5 * (ply.speedrun_mul or 1)
-		end
-	end)
-end
+hook.Add("TTTPlayerSpeedModifier", "SpeedrunModifySpeed", function(ply, _, _, refTbl)
+	if not IsValid(ply) or not ply:HasEquipmentItem("item_ttt_speedrun") then return end
+
+	refTbl[1] = refTbl[1] * 1.5 * (ply.speedrun_mul or 1)
+end)
